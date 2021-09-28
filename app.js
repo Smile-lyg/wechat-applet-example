2f3587ae5d63d22e374daef38156a2270b06b2d5
Smile-lyg <1921243831@qq.com>
2021-09-28 20:18:57 +0800

    小程序生命周期与页面生命周期

--- app.js
+++ app.js
@@ -3,18 +3,28 @@
 import './utils/index'
 
 App({
-  onLaunch() {
-    // 展示本地存储能力
-    const logs = wx.getStorageSync('logs') || []
-    logs.unshift(Date.now())
-    wx.setStorageSync('logs', logs)
-
-    // 登录
-    wx.login({
-      success: res => {
-        // 发送 res.code 到后台换取 openId, sessionKey, unionId
-      }
-    })
+  // 小程序初始化加载
+  onLaunch (options) {
+    // Do something initial when launch.
+    // 比如发送请求，获取一些全局的配置
+    console.log('小程序加载中');
+  },
+  // 小程序显示
+  onShow (options) {
+    // Do something when show.
+    // 比如说获取一些最新的后台信息
+    console.log('小程序显示');
+  },
+  // 小程序隐藏(被关闭)
+  onHide () {
+    // Do something when hide.
+    // 比如清除小程序的缓存数据
+    console.log('小程序隐藏');
+  },
+  // 小程序发生错误
+  onError (msg) {
+    // 比如当小程序页面发生错误时，跳转到一个友好的页面
+    console.log(msg)
   },
   globalData: {
     userInfo: null
--- pages/order/order.js
+++ pages/order/order.js
@@ -42,6 +42,7 @@ Page({
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
+    console.log('页面加载中');
     this.getSubject()
   },
 
@@ -49,34 +50,38 @@ Page({
    * 生命周期函数--监听页面初次渲染完成
    */
   onReady: function () {
-
+    // 只在页面第一次加载时 调用一次
+    // 一般用来获取一些不需要每次显示页面都要刷新的数据
+    console.log('页面初次渲染完成');
   },
 
   /**
    * 生命周期函数--监听页面显示
    */
   onShow: function () {
-
+    // 每次页面显示调用
+    console.log('页面显示');
   },
 
   /**
    * 生命周期函数--监听页面隐藏
    */
   onHide: function () {
-
+    console.log('页面隐藏');
   },
 
   /**
    * 生命周期函数--监听页面卸载
    */
   onUnload: function () {
-
+    console.log('页面卸载');
   },
 
   /**
    * 页面相关事件处理函数--监听用户下拉动作
    */
   onPullDownRefresh: function () {
+    console.log('用户下拉');
     // 下拉刷新数据
     // 页码重置
     this.data.pageIndex = 1
@@ -94,6 +99,7 @@ Page({
    * 页面上拉触底事件的处理函数
    */
   onReachBottom: function () {
+    console.log('页面上拉触底');
     // 上拉获取更多
     // 页码加一
     this.data.pageIndex ++
@@ -105,6 +111,6 @@ Page({
    * 用户点击右上角分享
    */
   onShareAppMessage: function () {
-
+    console.log('用户点击右上角分享');
   }
 })
